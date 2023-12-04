3ac0fc906d3af45087f97c2e2594d285666e2d5b
ahmed refay <ahmedrefay3000@gmail.com>
2023-12-04 16:43:08 +0200

    logo

--- app/invoices/[slug]/Logo.tsx
+++ app/invoices/[slug]/Logo.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Logo = () => {
-  return (
-    <div className='h-32 w-full bg-blue-700'>Logo</div>
-  )
-}
+    return (
+        <div className="h-32 w-full bg-stone-400 flex items-center justify-between p-4">
+            <div className="text-4xl text-center">
+                شركة مصانع رمضان للصناعات  المعدنية
+            </div>
+            <div>info</div>
+        </div>
+    );
+};
 
-export default Logo
+export default Logo;
