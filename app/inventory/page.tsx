713182d8221b4211871f6141d2ce5f4f36273c9c
ahmed refay <ahmedrefay3000@gmail.com>
2023-12-14 16:51:19 +0200

    inventory

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -22,18 +22,63 @@ const page = async () => {
         },
     });
 
-    // Flatten the array of line items into a single array of line items
-    const allLineItems = LineItems.flatMap((invoice) => invoice.lineItems);
+    async function getAvailableProducts() {
+        try {
+            const availableProducts = await prismaDb.product.findMany({
+                include: {
+                    Inventory: true,
+                    ProductionEvent: true,
+                    LineItem: {
+                        include: {
+                            invoice: true,
+                            ReturnedInvoice: true,
+                        },
+                    },
+                },
+            });
 
-    console.log(allLineItems);
+            const productsWithAvailability = availableProducts.map(
+                (product) => {
+                    const soldQuantity = product.LineItem.reduce(
+                        (total, lineItem) =>
+                            total + (lineItem.invoice ? lineItem.quantity : 0),
+                        0
+                    );
 
-    // Calculate the sum of quantities
-    const totalQuantity = allLineItems.reduce(
-        (total, currentItem) => total + currentItem.quantity,
-        0
-    );
+                    const returnedQuantity = product.LineItem.reduce(
+                        (total, lineItem) =>
+                            total +
+                            (lineItem.ReturnedInvoice ? lineItem.quantity : 0),
+                        0
+                    );
 
-    console.log("Total Quantity:", totalQuantity);
+                    const producedQuantity = product.ProductionEvent.reduce(
+                        (total, event) => total + event.quantity,
+                        0
+                    );
+
+                    const availableQuantity =
+                        producedQuantity - (soldQuantity + returnedQuantity);
+
+                    return {
+                        productName: product.name,
+                        availableQuantity,
+                    };
+                }
+            );
+
+            return productsWithAvailability;
+        } catch (error) {
+            console.error("Error retrieving available products:", error);
+            throw error;
+        } finally {
+            await prismaDb.$disconnect();
+        }
+    }
+
+    // Call the function to get available products
+    await getAvailableProducts();
+    console.log(await getAvailableProducts());
 
     return (
         <div>
