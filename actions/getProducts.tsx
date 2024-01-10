c8a35cddb73a08bf1d9614f6dcd51790a307ee9b
ahmed refay <ahmedrefay3000@gmail.com>
2024-01-10 08:34:45 +0200

    Get products From external api

---
+++ actions/getProducts.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const GetProducts = async () => {
+    const res = await axios.get(
+        "https://last-app-1nm1.vercel.app/api/dd55814a-7978-4caf-85af-1d4f60b73959/products"
+    );
+
+    const products = res.data.map((i: any) => {
+        return {
+            name: i.name,
+            featured: i.isFeatured,
+            category: i.category.name,
+            image: i.images[0].url,
+        };
+    });
+    console.log(products);
+    return products;
+};
