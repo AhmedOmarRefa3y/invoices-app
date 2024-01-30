535890d95bb13f76f1c3787a050542da29de11fa
ahmed refay <ahmedrefay3000@gmail.com>
2024-01-30 15:57:08 +0200

    revalidateApp button

--- actions/index.ts
+++ actions/index.ts
@@ -1,53 +1,8 @@
 "use server";
 
 import prismaDb from "@/lib/prisma";
-
+import { revalidateApp } from "./customer";
 export async function DbEdit() {
-    // const orderItems = await prismaDb.orderItem.deleteMany();
-    // const invlineItems = await prismaDb.lineItem.findMany({
-    //     where: {
-    //         invoiceId: {
-    //             not: null,
-    //         },
-    //         productId: {
-    //             not: undefined,
-    //         },
-    //     },
-    // });
-    // const retInvlineItem = await prismaDb.lineItem.findMany({
-    //     where: {
-    //         returnedInvoiceId: {
-    //             not: null,
-    //         },
-    //     },
-    // });
-
-    // const orderItems = await prismaDb.orderItem.createMany({
-    //     data: invlineItems.map((item) => {
-    //         return {
-    //             amount: item.amount as number,
-    //             price: item.price as number,
-    //             quantity: item.quantity,
-    //             invoiceId: item.invoiceId,
-    //             OrderNumber: item.ItemNumber as number,
-    //             productId: item.productId,
-    //         };
-    //     }),
-    // });
-
-    // console.log(invlineItems.count);
-    // console.log(orderItems.count);
-    // console.log(invlineItems.length);
-    // console.log(retInvlineItem.length);
-
-    // const itemsWithoutRelations = await prismaDb.lineItem.findMany({
-    //     where: {
-    //         returnedInvoiceId: null,
-    //         productionEventId: null,
-    //         invoiceId: null,
-    //     },
-    // });
-
-    // console.log(itemsWithoutRelations);
     console.log("edit db run");
+    revalidateApp();
 }
