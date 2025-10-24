4f5a5a19b341be45836e3ccfde4879f815fe81c6
Ahmad Omar <ahmedrefay3000@gmail.com>
2025-10-24 22:43:15 +0300

    feat: Refactor PurchasesInvoices component to use Suspense for loading state and improve data fetching logic

--- src/app/[locale]/(dashboard)/[orgid]/(invoices)/(showInvoices)/purchases_invocies/page.tsx
+++ src/app/[locale]/(dashboard)/[orgid]/(invoices)/(showInvoices)/purchases_invocies/page.tsx
@@ -1,11 +1,13 @@
+import { Suspense } from "react";
 import { TableUi } from "@/components/table";
 import { GetPurchasesInvoices } from "./PurchasesInvoices-utils";
 import { PurchasesCloumns } from "./PurchasesCloumns";
 import { getTranslations } from "next-intl/server";
+import Loading from "@/app/[locale]/(dashboard)/[orgid]/loading";
 
-const PurchasesInvoices = async ({ params }: { params: { orgid: string; locale: string } }) => {
+const PurchasesInvoicesContent = async ({ orgid }: { orgid: string }) => {
   const t = await getTranslations("PurchasesCloumns");
-  const PurchasesData = await GetPurchasesInvoices(params.orgid);
+  const PurchasesData = await GetPurchasesInvoices(orgid);
 
   const csvData = PurchasesData.map((item) => {
     return {
@@ -21,19 +23,27 @@ const PurchasesInvoices = async ({ params }: { params: { orgid: string; locale: 
     };
   });
   return (
+    <TableUi
+      columns={PurchasesCloumns}
+      data={PurchasesData}
+      filterAccessorKey="SupplierName"
+      filterlabel={t("supplierName")}
+      filterplaceholder={t("searchByName")}
+      notfound={t("noInvoicesFound")}
+      reversedNavButton={true}
+      csvData={csvData}
+      csvFileName="purchasesInvoices"
+    />
+  );
+};
+
+const PurchasesInvoices = async ({ params }: { params: { orgid: string; locale: string } }) => {
+  return (
     <div className="flex relative gap-2 max-h-screen h-full  px-2 max-w-full">
       <div className="w-fit mx-auto">
-        <TableUi
-          columns={PurchasesCloumns}
-          data={PurchasesData}
-          filterAccessorKey="SupplierName"
-          filterlabel={t("supplierName")}
-          filterplaceholder={t("searchByName")}
-          notfound={t("noInvoicesFound")}
-          reversedNavButton={true}
-          csvData={csvData}
-          csvFileName={"purchasesInvoices"}
-        />
+        <Suspense fallback={<Loading />}>
+          <PurchasesInvoicesContent orgid={params.orgid} />
+        </Suspense>
       </div>
     </div>
   );
