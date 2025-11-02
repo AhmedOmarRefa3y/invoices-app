edc5ac24b9c2696daed6ab2859cdd3a5a2c5b935
AhmedOmar <ahmedrefay3000@gmail.com>
2025-11-02 15:48:54 +0200

    fix : upgrade bugs fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,31 +1,6 @@
-import { getNextOrganizationNumber } from "@/actions";
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export const getNextInvoiceNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastInvoiceNumber");
-
-export const getNextPaymentNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastPaymentNumber");
-
-export const getNextProductionPlanNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastProductionPlanNumber");
-
-export const getNextProductionEventNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastProductionEventNumber");
-
-export const getNextPurchaseInvoiceNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastPurchaseInvoiceNumber");
-
-export const getNextReturnedInvoiceNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastReturnedInvoiceNumber");
-
-export const getNextPaymentToSupplierNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastPaymentToSupplierNumber");
-
-export const getNextJournalEntryNumber = (organizationId: string) =>
-  getNextOrganizationNumber(organizationId, "lastJournalEntryNumber");
